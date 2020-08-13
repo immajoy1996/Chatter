7952aef69af812a7bdea30ea92ba4ab5d302e3b7
Immanuel <immajoy1996@gmail.com>
2020-08-12 21:37:39 -0400

    setup sentence matching

--- app/src/main/java/com/example/chatter/ui/SpeechGame.kt
+++ app/src/main/java/com/example/chatter/ui/SpeechGame.kt
@@ -1,25 +1,15 @@
 package com.example.chatter.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.widget.EditText
-import android.widget.Toast
 import com.example.chatter.R
-import com.example.chatter.ui.activity.BaseActivity
 import com.example.chatter.ui.activity.BaseChatActivity
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_speech_game.*
-import kotlinx.android.synthetic.main.fragment_enter_username.*
 import java.util.*
 
 class SpeechGameActivity : BaseChatActivity(), TextToSpeech.OnInitListener {
     private var sentenceArray = arrayListOf<String>()
     private var currentIndex = 0
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
