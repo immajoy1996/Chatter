813a048e3822cab62f9159a7384ba9c8c68eead1
Immanuel Joy <immanueljoy@fb.com>
2020-11-16 20:02:01 -0500

    fine tuned story mode

--- app/src/main/java/com/example/chatter/ui/activity/StoryPathActivity.kt
+++ app/src/main/java/com/example/chatter/ui/activity/StoryPathActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.example.chatter.R
+import com.example.chatter.data.StoryPathInfo
 import com.example.chatter.ui.fragment.BaseFragment
 import com.example.chatter.ui.fragment.StoryPathFragment
 import kotlinx.android.synthetic.main.activity_bot_story_latest.*
@@ -15,11 +16,14 @@ import kotlinx.android.synthetic.main.top_bar.*
 class StoryPathActivity : BaseActivity() {
 
     val fragmentArray = arrayListOf<StoryPathFragment>()
+    val storyPathFragment1=StoryPathFragment.newInstance(arrayListOf<StoryPathInfo>())
+    val storyPathFragment2=StoryPathFragment.newInstance(arrayListOf<StoryPathInfo>())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_story_path)
         setUpTopBar()
+        setUpFragmentArray()
         setUpViewPager()
     }
 
@@ -27,6 +31,11 @@ class StoryPathActivity : BaseActivity() {
         home.visibility = View.GONE
         back.visibility = View.VISIBLE
         top_bar_title.text = "Story Mode"
+    }
+    
+    private fun setUpFragmentArray(){
+        fragmentArray.add(storyPathFragment1)
+        fragmentArray.add(storyPathFragment2)
     }
 
     private fun setUpViewPager() {
